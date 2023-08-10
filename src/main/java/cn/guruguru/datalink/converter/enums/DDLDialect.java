840d00d95fbd136ffa5693212336ba3620e4b804
JinsYin <jinsyin@163.com>
2023-08-10 14:43:56 +0800

    :sparkles: No longer support converting MySQL DDL

--- src/main/java/cn/guruguru/datalink/converter/enums/DDLDialect.java
+++ src/main/java/cn/guruguru/datalink/converter/enums/DDLDialect.java
@@ -1,11 +1,10 @@
 package cn.guruguru.datalink.converter.enums;
 
-import cn.guruguru.datalink.protocol.node.extract.scan.MySqlScanNode;
 import cn.guruguru.datalink.protocol.node.extract.scan.OracleScanNode;
 
 public enum DDLDialect {
     Oracle(OracleScanNode.TYPE),
-    MySQL(MySqlScanNode.TYPE),
+    // MySQL(MySqlScanNode.TYPE),
     ;
 
     private final String nodeType;
