7f63ebd1f06ad7d880c89695bd2ebfc1daa5ce56
JinsYin <jinsyin@163.com>
2023-09-04 20:03:08 +0800

    :truck: Rename the OracleUtil the JdbcUrlUtil

--- src/main/java/cn/guruguru/datalink/utils/JdbcUrlUtil.java
+++ src/main/java/cn/guruguru/datalink/utils/JdbcUrlUtil.java
@@ -3,22 +3,24 @@ package cn.guruguru.datalink.utils;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class OracleUtil {
+public class JdbcUrlUtil {
 
     final static public String ORACLE_URI_REGEX = "jdbc:oracle:thin:@(?://)?([.\\w]+):(\\d+)[:/](\\w+)";
     final static public Pattern ORACLE_URI_PATTERN = Pattern.compile(ORACLE_URI_REGEX);
 
+    // ~ extractor for oracle ---------------------------------------
+
     /**
-     * 从 Oracle JDBC URL 解析获取主机名
+     * Extract hostname from an oracle jdbc url
      *
-     * <p> SID 格式：{@code jdbc:oracle:thin:@[HOST][:PORT]:SID}
-     * <p> ServiceName 格式：{@code jdbc:oracle:thin:@//[HOST][:PORT]/SERVICE}
+     * <p>SID format: {@code jdbc:oracle:thin:@[HOST][:PORT]:SID}
+     * <p>ServiceName format: {@code jdbc:oracle:thin:@//[HOST][:PORT]/SERVICE}
      *
      * @see <a href="https://stackoverflow.com/a/12734630">How to parse a JDBC url to get hostname,port etc</a>
      * @param url Oracle JDBC URL
      * @return URL host
      */
-    public static String getHostFromUrl(String url) {
+    public static String extractHostFromOracleUrl(String url) {
         Matcher matcher = ORACLE_URI_PATTERN.matcher(url);
         if (matcher.find()) {
             return matcher.group(1);
@@ -27,12 +29,12 @@ public class OracleUtil {
     }
 
     /**
-     * 从 Oracle JDBC URL 解析获取端口号
+     * Extract port from an oracle jdbc url
      *
      * @param url Oracle JDBC URL
      * @return URL port
      */
-    public static int getPortFromUrl(String url) {
+    public static int extractPortFromOracleUrl(String url) {
         Matcher matcher = ORACLE_URI_PATTERN.matcher(url);
         if (matcher.find()) {
             return Integer.parseInt(matcher.group(2));
@@ -41,16 +43,19 @@ public class OracleUtil {
     }
 
     /**
-     * 从 Oracle JDBC URL 解析获取数据库（SID 或 ServiceName）
+     * Extract database(SID or ServiceName) from an oracle jdbc url
      *
      * @param url Oracle JDBC URL
      * @return  database name
      */
-    public static String getDatabaseFromUrl(String url) {
+    public static String extractDatabaseFromOracleUrl(String url) {
         Matcher matcher = ORACLE_URI_PATTERN.matcher(url);
         if (matcher.find()) {
             return matcher.group(3);
         }
         throw new IllegalArgumentException("The Oracle URI '" + url + "' is invalid");
     }
+
+    // ~ parser for mysql ------------------------------------------
+
 }
