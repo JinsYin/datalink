acf23f80572ddb2d33d0ce226650732e8ce26585
JinsYin <jinsyin@163.com>
2023-09-05 18:27:48 +0800

    :sparkles: Add some members for the KafkaNode

--- src/main/java/cn/guruguru/datalink/protocol/node/extract/cdc/KafkaNode.java
+++ src/main/java/cn/guruguru/datalink/protocol/node/extract/cdc/KafkaNode.java
@@ -2,33 +2,150 @@ package cn.guruguru.datalink.protocol.node.extract.cdc;
 
 import cn.guruguru.datalink.datasource.NodeDataSource;
 import cn.guruguru.datalink.datasource.DataSourceType;
+import cn.guruguru.datalink.protocol.Metadata;
+import cn.guruguru.datalink.protocol.enums.MetaKey;
+import cn.guruguru.datalink.protocol.field.DataField;
+import cn.guruguru.datalink.protocol.field.WatermarkField;
+import cn.guruguru.datalink.protocol.node.extract.CdcExtractNode;
+import com.google.common.base.Preconditions;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonCreator;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonInclude;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonTypeName;
-import org.apache.inlong.sort.protocol.FieldInfo;
 import org.apache.inlong.sort.protocol.enums.KafkaScanStartupMode;
-import org.apache.inlong.sort.protocol.node.extract.KafkaExtractNode;
-import org.apache.inlong.sort.protocol.node.format.Format;
-import org.apache.inlong.sort.protocol.transformation.WatermarkField;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.Serializable;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Kafka Streaming Node
  *
+ * @see org.apache.inlong.sort.protocol.node.extract.KafkaExtractNode
+ * @see <a href="https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/table/kafka/">Kafka SQL Connector</a>
+ * @see <a href="https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/table/upsert-kafka/">Upsert Kafka SQL Connector</a>
  * @see <a href="https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/table/kafka/#bounded-ending-position">Bounded Ending Position</a>
  */
+@EqualsAndHashCode(callSuper = true)
 @JsonTypeName(KafkaNode.TYPE)
 @NodeDataSource(DataSourceType.KAFKA_2X)
-public class KafkaNode extends KafkaExtractNode {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class KafkaNode extends CdcExtractNode implements Metadata, Serializable {
     public static final String TYPE = "Kafka";
+    @JsonProperty("topic")
+    private String topic;
+    @JsonProperty("bootstrapServers")
+    private String bootstrapServers;
+    @JsonProperty("groupId")
+    private String groupId;
+    @JsonProperty("format")
+    private String format; // InLong: Format
+    @JsonProperty("scanStartupMode")
+    private KafkaScanStartupMode scanStartupMode;
+    @JsonProperty("scanStartupSpecificOffsets")
+    private String scanStartupSpecificOffsets;
+    @JsonProperty("scanStartupTimestampMillis")
+    private String scanStartupTimestampMillis;
+    @JsonProperty("primaryKey")
+    private String primaryKey;
 
-    public KafkaNode(String id, String name, List<FieldInfo> fields, @Nullable WatermarkField watermarkField, Map<String, String> properties, @Nonnull String topic, @Nonnull String bootstrapServers, @Nonnull Format format, KafkaScanStartupMode kafkaScanStartupMode, String primaryKey, String groupId) {
-        super(id, name, fields, watermarkField, properties, topic, bootstrapServers, format, kafkaScanStartupMode, primaryKey, groupId);
+    @JsonCreator
+    public KafkaNode(@JsonProperty("id") String id,
+                            @JsonProperty("name") String name,
+                            @JsonProperty("fields") List<DataField> fields,
+                            @Nullable @JsonProperty("watermarkField") WatermarkField watermarkField,
+                            @JsonProperty("properties") Map<String, String> properties,
+                            @Nonnull @JsonProperty("topic") String topic,
+                            @Nonnull @JsonProperty("bootstrapServers") String bootstrapServers,
+                            @Nonnull @JsonProperty("format") String format,
+                            @JsonProperty("scanStartupMode") KafkaScanStartupMode scanStartupMode,
+                            @JsonProperty("primaryKey") String primaryKey,
+                            @JsonProperty("groupId") String groupId,
+                            @JsonProperty("scanStartupSpecificOffsets") String scanStartupSpecificOffsets,
+                            @JsonProperty("scanStartupTimestampMillis") String scanStartupTimestampMillis) {
+        super(id, name, fields, properties, watermarkField);
+        this.topic = Preconditions.checkNotNull(topic, "kafka topic is empty");
+        this.bootstrapServers = Preconditions.checkNotNull(bootstrapServers, "kafka bootstrapServers is empty");
+        this.format = Preconditions.checkNotNull(format, "kafka format is empty");
+        this.scanStartupMode = Preconditions.checkNotNull(scanStartupMode, "kafka scanStartupMode is empty");
+        this.primaryKey = primaryKey;
+        this.groupId = groupId;
+        if (scanStartupMode == KafkaScanStartupMode.SPECIFIC_OFFSETS) {
+            Preconditions.checkArgument(StringUtils.isNotEmpty(scanStartupSpecificOffsets),
+                    "scanStartupSpecificOffsets is empty");
+            this.scanStartupSpecificOffsets = scanStartupSpecificOffsets;
+        }
+        if (scanStartupMode == KafkaScanStartupMode.TIMESTAMP_MILLIS) {
+            Preconditions.checkArgument(StringUtils.isNotBlank(scanStartupTimestampMillis),
+                    "scanStartupTimestampMillis is empty");
+            this.scanStartupTimestampMillis = scanStartupTimestampMillis;
+        }
     }
 
-    public KafkaNode(String id, String name, List<FieldInfo> fields, @Nullable WatermarkField watermarkField, Map<String, String> properties, @Nonnull String topic, @Nonnull String bootstrapServers, @Nonnull Format format, KafkaScanStartupMode kafkaScanStartupMode, String primaryKey, String groupId, String scanSpecificOffsets, String scanTimestampMillis) {
-        super(id, name, fields, watermarkField, properties, topic, bootstrapServers, format, kafkaScanStartupMode, primaryKey, groupId, scanSpecificOffsets, scanTimestampMillis);
+    @Override
+    public Map<String, String> tableOptions() {
+        Map<String, String> options = super.tableOptions();
+        options.put("connector", "kafka");
+        options.put("topic", topic);
+        options.put("properties.bootstrap.servers", bootstrapServers);
+        options.put("scan.startup.mode", scanStartupMode.getValue());
+        if (StringUtils.isNotEmpty(scanStartupSpecificOffsets)) {
+            options.put("scan.startup.specific-offsets", scanStartupSpecificOffsets);
+        }
+        if (StringUtils.isNotBlank(scanStartupTimestampMillis)) {
+            options.put("scan.startup.timestamp-millis", scanStartupTimestampMillis);
+        }
+        if (StringUtils.isNotEmpty(groupId)) {
+            options.put("properties.group.id", groupId);
+        }
+        return options;
+    }
+
+    @Override
+    public String genTableName() {
+        return topic;
+    }
+
+    @Override
+    public String getPrimaryKey() {
+        return primaryKey;
+    }
+
+    @Override
+    public List<DataField> getPartitionFields() {
+        return super.getPartitionFields();
+    }
+
+    @Override
+    public boolean isVirtual(MetaKey metaKey) {
+        switch (metaKey) {
+            case KEY:
+            case VALUE:
+            case HEADERS:
+            case HEADERS_TO_JSON_STR:
+            case PARTITION:
+            case OFFSET:
+            case TIMESTAMP:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public Set<MetaKey> supportedMetaFields() {
+        return EnumSet.of(MetaKey.PROCESS_TIME, MetaKey.TABLE_NAME, MetaKey.OP_TYPE, MetaKey.DATABASE_NAME,
+                MetaKey.SQL_TYPE, MetaKey.PK_NAMES, MetaKey.TS, MetaKey.OP_TS, MetaKey.IS_DDL,
+                MetaKey.MYSQL_TYPE, MetaKey.BATCH_ID, MetaKey.UPDATE_BEFORE,
+                MetaKey.KEY, MetaKey.VALUE, MetaKey.PARTITION, MetaKey.HEADERS,
+                MetaKey.HEADERS_TO_JSON_STR, MetaKey.OFFSET, MetaKey.TIMESTAMP);
     }
 }
