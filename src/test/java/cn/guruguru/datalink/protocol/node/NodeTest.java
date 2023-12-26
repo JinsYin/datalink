c3357d129e7c2a817800aea66246dbe3d108b5e7
JinsYin <jinsyin@163.com>
2023-12-26 20:45:25 +0800

    :white_check_mark: Improve the NodeTest

--- src/test/java/cn/guruguru/datalink/protocol/node/NodeTest.java
+++ src/test/java/cn/guruguru/datalink/protocol/node/NodeTest.java
@@ -4,6 +4,8 @@ import cn.guruguru.datalink.protocol.node.extract.scan.MySqlScanNode;
 
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
 
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,9 +14,12 @@ public class NodeTest {
     public void deserialize() throws JsonProcessingException {
     String json =
         "{\"type\":\"MysqlScan\",\"id\":\"N10381712676128\",\"name\":\"N10381712676128\",\"primaryKey\":\"\",\"url\":\"jdbc:mysql://localhost:3306/mydatabase\",\"username\":\"rqyin\",\"password\":\"easipass\",\"tableName\":\"lake_policy\",\"properties\":{\"a\":1},\"fields\":[{\"nodeId\":\"N10381712676128\",\"type\":\"DataField\",\"name\":\"id\",\"dataType\":{\"type\":\"INT\"}}]}";
-        // Automatically convert to the MySqlScanNode
-        MySqlScanNode node = (MySqlScanNode) Node.serialize(json);
-        Assert.assertEquals("N10381712676128", node.getId());
-        Assert.assertEquals("MysqlScan", node.getNodeType());
+        // Indirect convert to the MySqlScanNode
+        MySqlScanNode node1 = (MySqlScanNode) Node.serialize(json);
+        // Directly convert to the MySqlScanNode
+        MySqlScanNode node2 = new ObjectMapper().readValue(json, MySqlScanNode.class);
+        Assert.assertEquals("N10381712676128", node1.getId());
+        Assert.assertEquals("MysqlScan", node1.getNodeType());
+        Assert.assertEquals(node2.getNodeType(), node1.getNodeType());
     }
 }
