a376421682f24e3129be20b085840a9eec547f12
JinsYin <jinsyin@163.com>
2024-02-05 15:36:08 +0800

    :sparkles: Add SimpleParserFactory to simplify the creation of parsers

---
+++ src/main/java/cn/guruguru/datalink/parser/factory/SimpleParserFactory.java
@@ -0,0 +1,32 @@
+package cn.guruguru.datalink.parser.factory;
+
+import cn.guruguru.datalink.exception.UnsupportedEngineException;
+import cn.guruguru.datalink.parser.Parser;
+import cn.guruguru.datalink.parser.impl.FlinkSqlParser;
+import cn.guruguru.datalink.parser.impl.SparkSqlParser;
+
+/**
+ * Simple Parser Factory designed based on the Simple Factory Pattern
+ *
+ * Usages:
+ * <pre>
+ *     Parser parser = SimpleParserFactory.createParser("Spark");
+ *     ParseResult parseResult = parser.parser(linkInfo);
+ * </pre>
+ */
+public class SimpleParserFactory {
+
+    /**
+     * Creates a sql parser based on the type of computing engine
+     */
+    public static Parser createParser(String engineType) {
+        switch (engineType) {
+            case "Spark":
+                return new SparkSqlParser();
+            case "Flink":
+                return new FlinkSqlParser();
+            default:
+                throw new UnsupportedEngineException("Unsupported computing engine");
+        }
+    }
+}
